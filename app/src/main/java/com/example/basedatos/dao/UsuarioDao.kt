6ea421c6ba465fcce25ff39f3a8dc56892c21cb6
Emanuel1596 <emanuel676@comunidad.unam.mx>
2026-06-07 03:12:55 -0600

    Ejercicio 2 crear DAO de Usuario

---
+++ app/src/main/java/com/example/basedatos/dao/UsuarioDao.kt
@@ -0,0 +1,15 @@
+package com.example.basedatos.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.basedatos.model.Usuario
+
+@Dao
+interface UsuarioDao {
+    @Insert
+    suspend fun insertarUsuario(usuario: Usuario)
+
+    @Query("SELECT * FROM usuarios_table")
+    suspend fun obtenerUsuarios(): List<Usuario>
+}
